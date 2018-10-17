b677130d3c5504c859e1496f5e179496731cd3a4
talkasrul <talkasrul@gmail.com>
2018-10-18 03:34:43 +0700

    Icon for Recommendation

--- containers/Recommendation.js
+++ containers/Recommendation.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Card, CardItem, Text, Body } from "native-base";
+import { Container, Header, Content, Card, CardItem, Text, Body, Icon } from "native-base";
 import { View } from 'react-native'
 
 import ExpenseCard from '../components/ExpenseCard'
 
 class Recommendation extends Component {
+  static navigationOptions = {
+    title: 'Recomendation',
+    tabBarLabel: 'Recomendation',
+    tabBarVisible:true,
+    tabBarIcon: <Icon name='star' />  
+  }
   state = {  }
   render() {
     return (
