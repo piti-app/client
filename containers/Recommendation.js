5e625468522b7496197735b4d73b3aefdefaa938
talkasrul <talkasrul@gmail.com>
2018-10-18 03:43:33 +0700

    Make Header Text

--- containers/Recommendation.js
+++ containers/Recommendation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Card, CardItem, Text, Body, Icon } from "native-base";
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 import ExpenseCard from '../components/ExpenseCard'
 
@@ -14,11 +14,30 @@ class Recommendation extends Component {
   state = {  }
   render() {
     return (
-        <View>
-          <Text>recommendation</Text>
-        </View>
+      <Container>
+        <Text style={styles.title}>RECOMMENDATION</Text>
+        <Content style={styles.main}>
+          <Text>Halo</Text>
+        </Content>
+      </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  main: {
+    padding: 10
+  },
+  mb10: {
+    marginBottom: 10
+  },
+  title: {
+    fontSize: 32,
+    fontFamily : 'bebaskai',
+    textAlign : 'center',
+    paddingTop: 20,
+    paddingBottom: 20,
+  }
+})
 
 export default Recommendation;
