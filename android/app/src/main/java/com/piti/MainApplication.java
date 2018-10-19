eac77e5fb05df8f6362d1e8358e524675bbf24b2
khodhirob <odirobbani@gmail.com>
2018-10-19 16:11:00 +0700

    alhamdulila

--- android/app/src/main/java/com/piti/MainApplication.java
+++ android/app/src/main/java/com/piti/MainApplication.java
@@ -3,6 +3,7 @@ package com.piti;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new VectorIconsPackage(),
             new RNFirebasePackage(),
             new RNFirebaseMessagingPackage(),
