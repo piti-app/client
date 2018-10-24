d6c4067fc39cdacf1f6ba32d3e42ef7949184b33
julianjca <julianjca11@gmail.com>
2018-10-24 10:32:16 +0700

    add from picture

--- App.js
+++ App.js
@@ -15,6 +15,8 @@ import { createBottomTabNavigator, createStackNavigator,SwitchNavigator } from '
 import { Icon } from 'native-base'
 import SplashScreen from 'react-native-splash-screen';
 import { fromLeft,zoomIn,flipY,fromTop } from 'react-navigation-transitions';
+import ImagePicker from 'react-native-image-picker';
+import axios from 'axios'
 
 import InitBudget from './containers/InitBudget'
 
@@ -23,10 +25,81 @@ import Add from './containers/Add'
 import Update from './components/FormUpdateExpense'
 import Recommendation from './containers/Recommendation'
 import Profile from './containers/Profile'
-import { isSignedIn,setFcm } from "./Authentication";
+import { isSignedIn,setFcm,getEmail,fcm } from "./Authentication";
 import SignedOut from './containers/Router'
 import EditProfile from './components/EditProfile'
+import getData from './store/actions/getData'
 
+const uploadReceipt = () =>{
+    const options = {
+        quality: 1.0,
+        maxWidth: 500,
+        maxHeight: 500,
+        storageOptions: {
+          skipBackup: true
+        }
+      };
+
+      ImagePicker.showImagePicker(options, (response) => {
+        console.log('Response = ', response);
+
+        if (response.didCancel) {
+          console.log('User cancelled photo picker');
+        }
+        else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        }
+        else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+        }
+        else {
+          const formData = new FormData
+          formData.append('image', {
+            uri : response.uri,
+            name: response.fileName,
+            type: response.type
+           })
+          axios.post('https://mtbcorner.wahyudisetiaji.xyz/articles/upload', formData,
+            {headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'multipart/form-data',
+            }}
+          )
+          .then((result) => {
+            fcm()
+            .then((fcmToken) => {
+                getEmail()
+                .then((email) => {
+                    console.log(email)
+                    axios({
+                        method : 'POST',
+                        url : `http://10.0.2.2:4000/expense/create/vision/${email}`,
+                        data: {
+                            url : result.data.link,
+                            fcmToken
+                        }
+                    })
+                    .then((response) => {
+                        console.log(response)
+                        store.dispatch(getData())
+                    }).catch((err) => {
+                        Alert.alert(JSON.stringify(err))
+                    });
+
+                }).catch((err) => {
+                    Alert.alert("Save Expense Error  ahaha!")
+                });
+
+            }).catch((err) => {
+                Alert.alert("Save Expense Error  ahahsdasdsa!")
+            });
+
+          }).catch((err) => {
+             console.log('Error Upload', err)
+          });
+        }
+      });
+}
 
 const StackHome = createStackNavigator({
     Home,
@@ -37,6 +110,7 @@ const StackHome = createStackNavigator({
     navigationOptions : ({ navigation }) => ({
         headerStyle: { backgroundColor: '#fff', elevation:0 },
         headerRight : (navigation.state.routeName==='Home') ? <Icon name="bookmarks" style={{ paddingRight : 30, paddingTop : 5 }} onPress={() => navigation.navigate('Add')} /> : <View></View>,
+        headerLeft : (navigation.state.routeName==='Home') ? <Icon name="camera" style={{ paddingLeft : 30, paddingTop : 5 }} onPress={uploadReceipt} /> : <View></View>
     }), transitionConfig: () => fromTop(1000)
 })
 
@@ -67,7 +141,7 @@ const SignedIn = createBottomTabNavigator({
         },
     }
 },{
-    
+
     tabBarOptions : {
         style : { borderTopColor: '#FFF', backgroundColor : '#FFF' }
     }})
@@ -106,8 +180,8 @@ export default class App extends Component {
             })
             .catch((err) => {Alert.alert(err)})
             let fcmToken = await firebase.messaging().getToken();
-            setFcm(fcmToken).then(result => {})                   
-        
+            setFcm(fcmToken).then(result => {})
+
         const notificationOpen: NotificationOpen = await firebase.notifications().getInitialNotification();
         if (notificationOpen) {
             const action = notificationOpen.action;
